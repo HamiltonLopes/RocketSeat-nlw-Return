06c9d10aedfc0cae2f90cf9572bfdc4eb46bc83c
HamiltonLopes <hamilton_dino02@hotmail.com>
2022-05-07 01:01:33 -0300

    fim da aula 4

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+window.addEventListener('scroll',onScroll);
+
+onScroll();
 function onScroll(){
+    showNavOnScroll();
+    showBackToTopOnScroll();
+}
+
+function showNavOnScroll(){
     if(scrollY > 0) navigation.classList.add('scroll');
     else navigation.classList.remove('scroll');
+}
+
+function showBackToTopOnScroll(){
+    if(scrollY > 800) backToTop.classList.add('show');
+    else backToTop.classList.remove('show');
 }
 
 function openMenu(){
@@ -10,3 +23,19 @@ function openMenu(){
 function closeMenu(){
     document.body.classList.remove('menu-expanded');
 }
+
+
+ScrollReveal({
+    distance: '30px',
+    origin: 'top',
+    duration: 600
+}).reveal(`#home,
+#home img,
+#home .stats,
+#services,
+#services header,
+#services .card,
+#about,
+#about header,
+#about .content
+`);
